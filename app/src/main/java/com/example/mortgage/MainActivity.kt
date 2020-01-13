f9aa7d7f274c700eb51a863634819c5691ac3fd6
alex-snoralex <58669283+alex-snoralex@users.noreply.github.com>
2020-01-12 23:20:17 -0800

    Cleaning and consolidating xml files

--- app/src/main/java/com/example/mortgage/MainActivity.kt
+++ app/src/main/java/com/example/mortgage/MainActivity.kt
@@ -60,7 +60,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun createListeners(){
-        fab.setOnClickListener { view ->
+        email.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
